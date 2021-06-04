88a508aca15d6ca178da9bc3c81c285f4c7a10a9
Anderson-Rodrigo <anderson.vargas90@gmail.com>
2021-06-04 16:01:54 -0300

    conectando no h2

--- backend/src/main/java/com/devsuperior/dsvendas/entities/Seller.java
+++ backend/src/main/java/com/devsuperior/dsvendas/entities/Seller.java
@@ -1,25 +1,51 @@
 package com.devsuperior.dsvendas.entities;
 
-import lombok.Data;
-
-import javax.persistence.Column;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.util.ArrayList;
 import java.util.List;
 
-@Data
 @Table(name = "tb_sellers")
+@Entity
 public class Seller {
 
 	@Id
-	@Column(name = "id_seller")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
-	@Column(name = "name")
 	private String name;
 
-	@OneToMany(mappedBy = "id_seler")
-	private List<Sale> saleList;
+	@OneToMany(mappedBy = "seller")
+	private List<Sale> sales = new ArrayList<>();
 
+	public Seller(){}
+
+	public Seller (Long id, String name, List<Sale> sales) {
+		this.id = id;
+		this.name = name;
+		this.sales = sales;
+	}
+
+	public Long getId () {
+		return id;
+	}
+
+	public void setId (Long id) {
+		this.id = id;
+	}
+
+	public String getName () {
+		return name;
+	}
+
+	public void setName (String name) {
+		this.name = name;
+	}
+
+	public List<Sale> sales () {
+		return sales;
+	}
+
+	public void setSales (List<Sale> sales) {
+		this.sales = sales;
+	}
 }
